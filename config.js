0368868a95ea860f19ef3f40e0bb3ae2a2d9759e
PemdesLainungan <sid.lainungan@gmail.com>
2023-06-25 15:55:12 +0800

    ADRIAN-BOT

---
+++ config.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '-' //https://api.lolhuman.xyz
+global.rosekey = '-' //https://api.itsrose.life
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'ARY-MD V2.1'
+global.namaowner = 'Aryxd お-ぎ'
+
+//—————「 Setting Owner 」—————//
+global.owner = '6285762008398'
+global.ownernomer = ["6285762008398"]
+global.premium = ['6285762008398']
+
+//—————「 Set Wm 」—————//
+global.packname = ''
+global.author = 'Aryxd お-ぎ'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    done: 'Done ✅',
+    admin: 'Feature Only for _*Admin Group*_',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: 'Feature Only for _*owner*_',
+    group: 'Feature Only for _*Group Chat*_',
+    private: 'Feature Only for _*Admin Group*_',
+    wait: 'Wait a Moment, for Process',
+    endLimit: 'Your imit has run out, Wait at 12 at night',
+    error: '*!!!Feature Error!!!*',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50,
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/d309b7e0643f28ffe403b.png'
+global.isLink = `https://youtube.com/@Aryjuanda`
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
